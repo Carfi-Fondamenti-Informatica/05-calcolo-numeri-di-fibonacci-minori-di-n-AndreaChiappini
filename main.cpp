123355705c0b556a33c4321af2aadd1a56448e25
AndreaChiappini <117998815+AndreaChiappini@users.noreply.github.com>
2022-11-28 08:32:29 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+    int main() {
+int n=0,x=1,y=0,z=0;
+cin >> n;
+        if (n>0){
+            cout << x << endl;
+            while (z<n){
+                z=x+y;
+                y=x;
+                x=z;
+                if(z<=n) {
+                    cout << z << endl;
+                }
+            }
+
 }
+        return 0;
+    }
